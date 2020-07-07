1da77bd1521211376c8162aa09795bebc4c92d70
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-07-07 21:40:08 +0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -16,43 +16,10 @@
         <input type="button" value="ok">
     </form>
     <?php
-   class Login
-   {
-       private $table;
-   
-       public function __construct()
-       {
-           $this->table = new DbEntity(
-               Config::MYSQL_TABLE,
-               DB::Link([
-                   'host' => Config::MYSQL_HOST,
-                   'username' => Config::MYSQL_USER_NAME,
-                   'password' => Config::MYSQL_PASSWORD,
-                   'dbname' => Config::MYSQL_DATABASE
-               ])
-           );
-       }
-   
-   //    public function userCheck(string $login, string $password): bool
-   //    {
-   //        return !empty($this->table->runSQL(
-   //            'SELECT*FROM form WHERE login="' . $login . '" AND password="' . $password . '"'
-   //        ));
-   //    }
-   
-       public function userCheck2(string $login, string $password): bool
-       {
-           return !empty($this
-               ->table
-               ->setWhere('login="' . $login . '" AND password="' . $password . '"')
-               ->get()
-           );
-       }
-   
-   }
-
-
-
+    class Login
+    {
+        
+    }
 
     ?>
 </body>
