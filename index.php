597924f233e479eb17a4b1cbc2187feabcda292e
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-07-07 21:01:11 +0300

    Just Composer

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-    <form action="">
+    <form action="?">
         <input type="text"><br>
         <input type="text" name="" id=""><br>
         <input type="button" value="ok">
